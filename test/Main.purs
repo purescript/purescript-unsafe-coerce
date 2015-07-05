ed88f0d526e5a6edf0cf5588229d10cf09d02dcb
Phil Freeman <paf31@cantab.net>
2015-07-04 19:40:58 -0700

    Add test

--- test/Main.purs
+++ test/Main.purs
@@ -1,6 +1,23 @@
 module Test.Main where
 
+import Prelude
+import Unsafe.Coerce (unsafeCoerce)
+
 import Control.Monad.Eff.Console
 
-main = do
-  log "You should add some tests."
+newtype Foo = Foo String
+
+newtype Bar = Bar String
+
+-- | The two newtypes `Foo` and `Bar` have the same runtime representation,
+-- | so it is safe to coerce one into the other directly.
+coerceFoo :: Foo -> Bar
+coerceFoo = unsafeCoerce
+
+-- | It is also safe to coerce entire collections, without having to map over
+-- | individual elements.
+coerceFoos :: forall f. (Functor f) => f Foo -> f Bar
+coerceFoos = unsafeCoerce
+
+main = case coerceFoos [Foo "Hello", Foo " ", Foo "World"] of
+         [Bar x, Bar y, Bar z] -> log (x <> y <> z)
