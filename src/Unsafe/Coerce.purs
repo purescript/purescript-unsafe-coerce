82043b97072357e7e21d2fb35b70adba0fa7adfa
JordanMartinez <jordanalex.martinez@gmail.com>
2020-10-07 08:29:03 -0700

    Updates library's documentation for v0.14.0 release (#11)
    * Remove examples of usage
    
    The removed documentation now applies to `purescript-safe-coerce`'s 
    `coerce` function.
    
    * `safe-coerce` post-v0.14.0 release now replaces some usages of unsafeCoerce
    
    * Update the 'define specializations' comment's example

--- src/Unsafe/Coerce.purs
+++ src/Unsafe/Coerce.purs
@@ -8,20 +8,20 @@ module Unsafe.Coerce
 -- | (that is, the caller's) responsibility to ensure that the underlying
 -- | representation for both types is the same.
 -- |
--- | One application for this function is to avoid doing work that you know is a
--- | no-op because of newtypes. For example, if you have an `Array (Conj a)` and you
--- | want an `Array (Disj a)`, you could do `Data.Array.map (runConj >>> Disj)`, but
--- | this performs an unnecessary traversal. `unsafeCoerce` accomplishes the same
--- | for free.
--- |
--- | It is highly recommended to define specializations of this function rather than
--- | using it as-is. For example:
+-- | Because this function is extraordinarily flexible, type inference
+-- | can greatly suffer. It is highly recommended to define specializations of
+-- | this function rather than using it as-is. For example:
 -- |
 -- | ```purescript
--- | mapConjToDisj :: forall a. Array (Conj a) -> Array (Disj a)
--- | mapConjToDisj = unsafeCoerce
+-- | fromBoolean :: Boolean -> Json
+-- | fromBoolean = unsafeCoerce
 -- | ```
 -- |
 -- | This way, you won't have any nasty surprises due to the inferred type being
 -- | different to what you expected.
+-- |
+-- | After the v0.14.0 PureScript release, some of what was accomplished via
+-- | `unsafeCoerce` can now be accomplished via `coerce` from
+-- | `purescript-safe-coerce`. See that library's documentation for more
+-- | context.
 foreign import unsafeCoerce :: forall a b. a -> b
